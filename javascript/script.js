56c0f6cbe4b1401825e1bb81968f19d9c9b30b34
rtavaresramos <rtavareskxd99@gmail.com>
2020-02-06 04:51:26 -0300

    Modal initied

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+var inputCheck = document.querySelector('input[type=checkbox]')
+var slider = document.getElementById('my-range')
+var output = document.getElementById('value')
+
+
+output.innerHTML - slider.value
+
+
+
+slider.oninput = function(){
+    
+    output.innerHTML = parseFloat(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    
+    var a = parseFloat(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    console.log(a)
+    
+}
+
+
+slider.addEventListener("mousemove", function(){
+    var x = slider.value / 100
+    var color = 'linear-gradient(90deg, rgb(24, 172, 196)'+ x +'%, rgb(204, 204,204)'+ x +'%)'
+    slider.style.background = color
+})
