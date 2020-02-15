87a5c3735c276a2f758c88e649527752f9bfa0f1
rtavaresramos <rtavareskxd99@gmail.com>
2020-02-15 10:44:02 -0300

    Delete button is already working

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+
+
+  function deleteCard(i) {
+    // var cardBtnId = document.querySelector("#danger-btn-"+i+"")
+    var cardId = document.querySelector("#added-card-"+ i +"")
+    cardId.remove()
+  }
+
+  
